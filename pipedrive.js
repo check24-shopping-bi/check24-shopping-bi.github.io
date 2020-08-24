570322d8dc3b8d2b9afddea9c4df866c8f2e21bc
check24-shopping-bi <68905410+check24-shopping-bi@users.noreply.github.com>
2020-08-24 11:29:40 +0200

    Update pipedrive.js

--- pipedrive.js
+++ pipedrive.js
@@ -8,41 +8,6 @@
 		var inputForm = JSON.parse(tableau.connectionData),
 			callbackTables = [];
 
-		var activitiesCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'company_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'done', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'type', dataType: tableau.dataTypeEnum.string },
-			{ id: 'reference_type', dataType: tableau.dataTypeEnum.string },
-			{ id: 'reference_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'due_date', dataType: tableau.dataTypeEnum.date },
-			{ id: 'due_time', dataType: tableau.dataTypeEnum.string },
-			{ id: 'duration', dataType: tableau.dataTypeEnum.string },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'marked_as_done_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'subject', dataType: tableau.dataTypeEnum.string },
-			{ id: 'deal_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'org_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'person_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'update_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'gcal_evevnt_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'google_calendar_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'google_calendar_etag', dataType: tableau.dataTypeEnum.string },
-			{ id: 'note', dataType: tableau.dataTypeEnum.string },
-			{ id: 'participant_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'participants', dataType: tableau.dataTypeEnum.string },
-			{ id: 'person_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'org_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'deal_title', dataType: tableau.dataTypeEnum.string },
-			{ id: 'assigned_to_user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'created_by_user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'owner_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'person_dropbox_bcc', dataType: tableau.dataTypeEnum.string },
-			{ id: 'deal_dropbox_bcc', dataType: tableau.dataTypeEnum.string },
-		];
-
 		var dealsCols = [
 			{ id: 'id', dataType: tableau.dataTypeEnum.int },
 			{ id: '62b493b099a8a35016ad9a200703cfaf58877f0c',alias:"ID_SHOP", dataType: tableau.dataTypeEnum.string },
@@ -64,208 +29,20 @@
 
 		];
 
-		var goalsCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'company_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'stage_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_goal_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'period', dataType: tableau.dataTypeEnum.string },
-			{ id: 'expected', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'goal_type', dataType: tableau.dataTypeEnum.string },
-			{ id: 'expected_sum', dataType: tableau.dataTypeEnum.int },
-			{ id: 'currency', dataType: tableau.dataTypeEnum.string },
-			{ id: 'expected_type', dataType: tableau.dataTypeEnum.string },
-			{ id: 'created_by_user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'pipeline_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'master_expected', dataType: tableau.dataTypeEnum.int },
-			{ id: 'delivered', dataType: tableau.dataTypeEnum.int },
-			{ id: 'delivered_sum', dataType: tableau.dataTypeEnum.int },
-			{ id: 'period_start', dataType: tableau.dataTypeEnum.date },
-			{ id: 'period_end', dataType: tableau.dataTypeEnum.date },
-			{ id: 'user_name', dataType: tableau.dataTypeEnum.string },
-			
-			
-
-		];
-
-		var organizationsCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'company_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'name', dataType: tableau.dataTypeEnum.string },
-		];
-
-		var notesCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'deal_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'person_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'org_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'content', dataType: tableau.dataTypeEnum.string },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'update_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'pinned_to_deal_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'pinned_to_person_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'pinned_to_organization_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'last_update_user_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'organization', dataType: tableau.dataTypeEnum.int },
-		];
-
-		var personsCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'owner_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'org_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'first_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'last_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'open_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'related_open_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'closed_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'related_closed_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'participant_open_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'participant_closed_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'email_messages_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'activities_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'done_activities_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'undone_activities_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'reference_activities_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'files_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'notes_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'followers_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'won_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'related_won_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'lost_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'related_lost_deals_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'home_phone', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'work_phone', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'mobile_phone', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'other_phone', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'home_email', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'work_email', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'other_email', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'first_char', dataType: tableau.dataTypeEnum.string },
-			{ id: 'update_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'visible_to', dataType: tableau.dataTypeEnum.string },
-			{ id: 'picture_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'next_activity_date', dataType: tableau.dataTypeEnum.date },
-			{ id: 'next_activity_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'next_activity_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'last_activity_date', dataType: tableau.dataTypeEnum.date },
-			{ id: 'last_activity_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'last_activity_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'last_incoming_mail_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'last_outgoing_mail_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'has_im', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'postal_address', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_subpremise', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_street_number', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_route', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_sublocality', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_locality', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_admin_area_level_1', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_admin_area_level_2', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_country', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_postal_code', dataType: tableau.dataTypeEnum.string },
-			{ id: 'postal_address_formatted_address', dataType: tableau.dataTypeEnum.string },
-			{ id: 'org_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'cc_email', dataType: tableau.dataTypeEnum.string },
-			{ id: 'owner_name', dataType: tableau.dataTypeEnum.string },
-		];
-
-		var productsCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'code', dataType: tableau.dataTypeEnum.string },
-			{ id: 'unit', dataType: tableau.dataTypeEnum.int },
-			{ id: 'tax', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'selectable', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'first_char', dataType: tableau.dataTypeEnum.string },
-			{ id: 'visible_to', dataType: tableau.dataTypeEnum.string },
-			{ id: 'owner_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'files_count', dataType: tableau.dataTypeEnum.int },
-			{ id: 'followers_count', dataType: tableau.dataTypeEnum.date },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'update_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'price_amount', alias: 'price', dataType: tableau.dataTypeEnum.int },
-			{ id: 'price_currency', dataType: tableau.dataTypeEnum.int },
-			{ id: 'price_cost', dataType: tableau.dataTypeEnum.int },
-			{ id: 'price_overhead_cost', dataType: tableau.dataTypeEnum.int },
-			{ id: 'variation_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'variation_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'variation_price', dataType: tableau.dataTypeEnum.int },
-			{ id: 'variation_comment', dataType: tableau.dataTypeEnum.string },
-
-		];
-
-		var stagesCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'order_nr', dataType: tableau.dataTypeEnum.int },
-			{ id: 'name', dataType: tableau.dataTypeEnum.int },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.string },
-			{ id: 'pipeline_id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'add_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'update_time', dataType: tableau.dataTypeEnum.date },
-			{ id: 'pipeline_name', dataType: tableau.dataTypeEnum.date },
-		];
-
 		// Schema for users
-		var usersCols = [
-			{ id: 'id', dataType: tableau.dataTypeEnum.int },
-			{ id: 'name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'default_currency', dataType: tableau.dataTypeEnum.string },
-			{ id: 'locale', dataType: tableau.dataTypeEnum.string },
-			{ id: 'lang', dataType: tableau.dataTypeEnum.int },
-			{ id: 'email', dataType: tableau.dataTypeEnum.string },
-			{ id: 'phone', dataType: tableau.dataTypeEnum.string },
-			{ id: 'activated', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'last_login', dataType: tableau.dataTypeEnum.date },
-			{ id: 'created', dataType: tableau.dataTypeEnum.date },
-			{ id: 'modified', dataType: tableau.dataTypeEnum.date },
-			{ id: 'signup_flow_variation', dataType: tableau.dataTypeEnum.string },
-			{ id: 'has_created_company', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'is_admin', dataType: tableau.dataTypeEnum.int },
-			{ id: 'timezone_name', dataType: tableau.dataTypeEnum.string },
-			{ id: 'timezone_offset', dataType: tableau.dataTypeEnum.string },
-			{ id: 'active_flag', dataType: tableau.dataTypeEnum.bool },
-			{ id: 'role_id', dataType: tableau.dataTypeEnum.string },
-			{ id: 'icon_url', dataType: tableau.dataTypeEnum.string },
-			{ id: 'is_you', dataType: tableau.dataTypeEnum.bool },
-		];
-
-
 		var categoryCols = [
 			{ id: 'id', dataType: tableau.dataTypeEnum.int },	
 			{ id: 'label', dataType: tableau.dataTypeEnum.string },	
 			
 		]
 
-		var activitiesTable = { id: 'activities', alias: 'activities', columns: activitiesCols };
 		var dealsTable = { id: 'deals', alias: 'deals', columns: dealsCols };
-		var goalsTable = { id: 'goals', alias: 'goals', columns: goalsCols };
-		var notesTable = { id: 'notes', alias: 'notes', columns: notesCols };
-		var organizationsTable = { id: 'organizations', alias: 'organizations', columns: organizationsCols };
-		var personsTable = { id: 'persons', alias: 'Persons', columns: personsCols };
-		var productsTable = { id: 'products', alias: 'products', columns: productsCols };
-		var stagesTable = { id: 'stages', alias: 'stages', columns: stagesCols };
-		var usersTable = { id: 'users', alias: 'users', columns: usersCols };
+
 		var categoryTable = { id: 'maincategory', alias: 'dealFields/12551', columns: categoryCols };
 
 
-		if (inputForm.tables.indexOf('activities') >= 0) callbackTables.push(activitiesTable);
 		if (inputForm.tables.indexOf('deals') >= 0) callbackTables.push(dealsTable);
-		if (inputForm.tables.indexOf('goals') >= 0) callbackTables.push(goalsTable);
-		if (inputForm.tables.indexOf('notes') >= 0) callbackTables.push(notesTable);
-		if (inputForm.tables.indexOf('organizations') >= 0) callbackTables.push(organizationsTable);
-		if (inputForm.tables.indexOf('persons') >= 0) callbackTables.push(personsTable);
-		if (inputForm.tables.indexOf('products') >= 0) callbackTables.push(productsTable);
-		if (inputForm.tables.indexOf('stages') >= 0) callbackTables.push(stagesTable);
-		if (inputForm.tables.indexOf('users') >= 0) callbackTables.push(usersTable);
-		callbackTables.push(categoryTable);
+
 
 
 		schemaCallback(callbackTables);
